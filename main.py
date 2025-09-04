4d17a1219d7256dcbae9b3f6daa2cae6f5e07aa2
iamayan-lal <ayanl0069@gmail.com>
2025-09-04 18:03:41 +0530

    DAY 24 : FILES & PATHS

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
+names = open("./Input/Names/invited_names.txt","r")
+names_list = names.readlines()
 
-player = Player()
-car_manager = CarManager()
-scoreboard = Scoreboard()
-
-screen.listen()
-screen.onkey(player.move, "Up")
-
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-    car_manager.create_car()
-    car_manager.move_cars()
-
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20 :
-            game_is_on = False
-            scoreboard.game_over()
-
-    if player.is_at_finish_point():
-        player.go_to_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+for i in range(0,len(names_list)):
+    names_list[i] = names_list[i].strip()
 
 
+letter = open("./Input/Letters/starting_letter.txt","r")
+temp =letter.read()
 
-screen.exitonclick()
+for name in names_list:
+    demo = open("./Output/ReadyToSend/"+i+".txt","w")
+    demo.write(temp.replace("[name]",name))
+    demo.close()
 
-
-# Create a turtle player that starts at the bottom of the screen and listen for the "Up" keypress to move the turtle north.
-# Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge of the screen. No cars should be generated in the top and bottom 50px of the screen (think of it as a safe zone for our little turtle). Hint: generate a new car only every 6th time the game loop runs.
-# Detect when the turtle player collides with a car and stop the game if this happens
-# Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When this happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about creating an attribute and using the MOVE_INCREMENT to increase the car speed.
-# Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre.
